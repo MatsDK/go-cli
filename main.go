b057e0ec7b1683223e926b54dd364627a742fdff
MatsDK <matsdekort257@gmail.com>
2021-10-07 16:38:37 +0000

    flagsets

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import(
 	"fmt"
 	"os"
+	"log"
+	"flag"
 )
 
 func main() {
@@ -13,13 +15,34 @@ func main() {
 
 	switch os.Args[1] {
 		case "set":
-			fmt.Println("set")
+			setCmd := flag.NewFlagSet("set", flag.ExitOnError)
+			brightnessFlag := setCmd.Int("brightness", 100, "the brightness")
+			rFlag := setCmd.Int("r", -1, "the red color")
+			gFlag := setCmd.Int("g", -1, "the green color")
+			bFlag := setCmd.Int("b", -1, "the blue color")
+			err := setCmd.Parse(os.Args[2:])
+
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			if *rFlag > 255 {
+				*rFlag = 255
+			}
+			if *gFlag > 255 {
+				*gFlag = 255
+			}
+			if *bFlag > 255 {
+				*bFlag = 255
+			}
+
+			fmt.Printf("set brightness: %d, red: %d, green: %d, blue: %d\n", *brightnessFlag, *rFlag, *gFlag, *bFlag)
 		case "on":
 			fmt.Println("on")
 		case "off":
 			fmt.Println("off")
 		default: 
 			fmt.Println("off | on | set")
-	}
 
+	}
 }
