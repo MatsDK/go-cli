d3a367280253c8da81be0caa439c4e06a7141143
MatsDK <matsdekort257@gmail.com>
2021-11-10 17:53:21 +0000

    add/delete presets

--- main.go
+++ main.go
@@ -1,92 +1,89 @@
 package main
 
-import(
-	"fmt"
-	"os"
-	"log"
+import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
+
 	D "./lib"
 )
 
 type Preset struct {
-	name string `json:"name"`
-	mode string `json:"mode"`
-	brightness int `json:"brightness"`
-	red int `json:"red"`
-	green int `json:"green"`
-	blue int `json:"blue"`
-
+	Name       string `json:"name"`
+	Mode       string `json:"mode"`
+	Brightness int    `json:"brightness"`
+	Red        int    `json:"red"`
+	Green      int    `json:"green"`
+	Blue       int    `json:"blue"`
 }
 
 func main() {
-	if len(os.Args) <  2 {
+	if len(os.Args) < 2 {
 		fmt.Println("off | on | set | preset")
 		return
 	}
 
 	switch os.Args[1] {
-		case "set":
-			if os.Args[2] == "preset" {
-				fmt.Println("set preset")
-				presetCmd := flag.NewFlagSet("preset", flag.ExitOnError)
-				nameFlag := presetCmd.String("name", "", "the name of the preset")
-				err := presetCmd.Parse(os.Args[3:])
+	case "set":
+		if os.Args[2] == "preset" {
+			fmt.Println("set preset")
+			presetCmd := flag.NewFlagSet("preset", flag.ExitOnError)
+			nameFlag := presetCmd.String("name", "", "the name of the preset")
+			err := presetCmd.Parse(os.Args[3:])
 
-				if err != nil {
-					log.Fatal(err.Error())
-				}
+			if err != nil {
+				log.Fatal(err.Error())
+			}
 
+			if len(*nameFlag) != 0 {
+				preset := getPresetByName(*nameFlag)
 
-				if len(*nameFlag) != 0 {
-					preset := getPresetByName(*nameFlag)
-
-					if len(preset.name) != 0 {
-						if preset.mode == "static" {
-							setStatic(preset.brightness, preset.red, preset.green, preset.blue)
-						}
+				if len(preset.Name) != 0 {
+					if preset.Mode == "static" {
+						setStatic(preset.Brightness, preset.Red, preset.Green, preset.Blue)
 					}
 				}
-
-
-			} else {
-				setCmd := flag.NewFlagSet("set", flag.ExitOnError)
-				brightnessFlag := setCmd.Int("brightness", 100, "the brightness")
-				rFlag := setCmd.Int("r", -1, "the red color")
-
-				gFlag := setCmd.Int("g", -1, "the green color")
-				bFlag := setCmd.Int("b", -1, "the blue color")
-				err := setCmd.Parse(os.Args[2:])
-
-				if err != nil {
-					log.Fatal(err.Error())
-				}
-
-				if *rFlag > 255 {
-					*rFlag = 255
-				}
-				if *gFlag > 255 {
-					*gFlag = 255
-				}
-				if *bFlag > 255 {
-					*bFlag = 255
-				}
-
-				setStatic(*brightnessFlag, *rFlag, *gFlag, *bFlag)
-			}
-		case "preset":
-			if len(os.Args) <= 2 {
-				fmt.Println("list | add | set | delete")
-				return
 			}
 
-			presetCmd := os.Args[2]
-			handlePreset(presetCmd)
-		case "on":
-			fmt.Println("on")
-		case "off":
-			fmt.Println("off")
-		default:
-			fmt.Println("off | on | set")
+		} else {
+			setCmd := flag.NewFlagSet("set", flag.ExitOnError)
+			brightnessFlag := setCmd.Int("brightness", 100, "the brightness")
+			rFlag := setCmd.Int("r", -1, "the red color")
+			gFlag := setCmd.Int("g", -1, "the green color")
+			bFlag := setCmd.Int("b", -1, "the blue color")
+			err := setCmd.Parse(os.Args[2:])
+
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			if *rFlag > 255 {
+				*rFlag = 255
+			}
+			if *gFlag > 255 {
+				*gFlag = 255
+			}
+			if *bFlag > 255 {
+				*bFlag = 255
+			}
+
+			setStatic(*brightnessFlag, *rFlag, *gFlag, *bFlag)
+		}
+	case "preset":
+		if len(os.Args) <= 2 {
+			fmt.Println("list | add | delete")
+			return
+		}
+
+		presetCmd := os.Args[2]
+		handlePreset(presetCmd)
+	case "on":
+		fmt.Println("on")
+	case "off":
+		fmt.Println("off")
+	default:
+		fmt.Println("off | on | set")
 
 	}
 }
@@ -99,31 +96,68 @@ func getPresetByName(presetName string) Preset {
 	query := fmt.Sprintf("SELECT name, mode, brightness, red, green, blue FROM preset WHERE name='%s';", presetName)
 	rows := D.Query(query, D.ConnectDB())
 
-	rows.Next() 
+	rows.Next()
 	var name, mode string
 	var brightness, red, green, blue int
 	rows.Scan(&name, &mode, &brightness, &red, &green, &blue)
 
-	return Preset{name: name, mode: mode, brightness: brightness, red: red, green: green, blue:blue}
+	return Preset{Name: name, Mode: mode, Brightness: brightness, Red: red, Green: green, Blue: blue}
 }
 
 func handlePreset(cmd string) {
-	db := D.ConnectDB()
-
 	switch cmd {
-		case "list":
-			rows, err := db.Query("SELECT * FROM preset;")
-			if err != nil {
-				panic(err)
-			}
+	case "list":
+		rows := D.Query("SELECT name FROM preset;", D.ConnectDB())
 
-			for rows.Next() {
-				var name string
-				rows.Scan(&name)
+		for rows.Next() {
+			var name string
+			rows.Scan(&name)
 
-				fmt.Println(name)
-			}
-		default:
-			fmt.Println("list | add | set| delete")
+			fmt.Println(name)
+		}
+	case "add":
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		nameFlag := addCmd.String("name", "", "the name of the preset")
+		modeFlag := addCmd.String("mode", "", "the mode of the preset")
+		brightnessFlag := addCmd.Int("brightness", 100, "the brightness")
+		rFlag := addCmd.Int("r", 0, "the red color")
+		gFlag := addCmd.Int("g", 0, "the green color")
+		bFlag := addCmd.Int("b", 0, "the blue color")
+		err := addCmd.Parse(os.Args[3:])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(*nameFlag) == 0 || len(*modeFlag) == 0 {
+			fmt.Println("You have to specify: name, mode, brightness, r, g, b")
+			return
+		}
+
+		if *modeFlag == "static" {
+			query := fmt.Sprintf("INSERT INTO preset(name, mode, brightness, red, green, blue) VALUES('%s', '%s', %d, %d, %d, %d);",
+				*nameFlag, *modeFlag, *brightnessFlag, *rFlag, *gFlag, *bFlag)
+
+			D.Query(query, D.ConnectDB())
+		}
+
+	case "delete":
+		removeCmd := flag.NewFlagSet("remove", flag.ExitOnError)
+		nameFlag := removeCmd.String("name", "", "the name of the preset to delete")
+		err := removeCmd.Parse(os.Args[3:])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(*nameFlag) == 0 {
+			log.Fatal("You have to provide a name of a preset")
+		}
+
+		query := fmt.Sprintf("DELETE FROM preset WHERE name='%s';", *nameFlag)
+		D.Query(query, D.ConnectDB())
+
+	default:
+		fmt.Println("list | add | delete")
 	}
 }
