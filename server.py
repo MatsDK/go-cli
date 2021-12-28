1e3a73518b76b4497dec759938f4482a98845a2d
MatsDK <matsdekort257@gmail.com>
2021-12-28 13:08:49 +0000

    flask api set color

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from rpi_ws281x import PixelStrip, Color
+import flask
 import time
 
 LED_COUNT = 60        
@@ -15,13 +16,31 @@ def colorWipe(strip, color, wait_ms=50):
         time.sleep(wait_ms / 1000.0)
         strip.show()
 
-if __name__ == '__main__':
-    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    strip.begin()
+def setColor(strip, color ):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        time.sleep(20 / 1000.0)
+        strip.show()
+    strip.show()
 
-    try:
-        while True:
-            colorWipe(strip, Color(0, 0, 255), 24) 
-            colorWipe(strip, Color(0, 0, 0), 24) 
-    except KeyboardInterrupt:
-        colorWipe(strip, Color(0, 0, 0), 10)
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/setStaticColor', methods=['POST'])
+def setStaticColor():
+    jsonBody = flask.request.get_json()
+    setColor(strip, Color(jsonBody["r"], jsonBody["g"], jsonBody["b"]))
+    return { "success": True } 
+
+try:
+    app.run(host="192.168.0.164")   
+except KeyboardInterrupt:
+    setColor(strip, Color(0, 0, 0))
+
+
+
+
+
